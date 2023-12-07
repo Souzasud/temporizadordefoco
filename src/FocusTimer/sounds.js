02998be81583159ad415031316bfa3542de3cfa8
CarlosSouza <133075307+Souzasud@users.noreply.github.com>
2023-12-06 23:24:51 -0300

    Add files via upload

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/Chuva.wav')
+
+export const kitchenTimer = new Audio('./assets/Cafeteria.wav')
+
+export const bgAudio = new Audio('./assets/Floresta.wav')
+
+//bgAudio.loop = true
